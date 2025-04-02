71f7b13dfc3007beb3df45d7f185767a534071fa
Jacob Rangel <jacobrangel0628@gmail.com>
2025-04-02 13:59:23 -0500

    Create template.tsx

---
+++ src/components/widgets/template.tsx
@@ -0,0 +1,12 @@
+
+export default function Template() {
+
+    return (
+        <div className=" bg-[#2e2e31] text-center text-2xl font-black p-10 rounded-xl">
+            Template
+            <div className="flex gap-16 items-center font-normal justify-center py-5">
+                
+            </div>
+        </div>
+    )
+}

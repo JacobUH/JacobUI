76caad841457387f94e4c63ab52e4f28c27d51df
Jacob Rangel <jacobrangel0628@gmail.com>
2025-04-02 14:07:31 -0500

    Create layout.tsx

---
+++ src/app/layout.tsx
@@ -0,0 +1,38 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "../../resources/globals.css";
+import StoreProvider from "./StoreProvider";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "JacobUI",
+  description: "A bunch of small projects and ideas!",
+  icons: "icons/jacobUI.svg"
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <StoreProvider>
+      <html lang="en">
+        <body
+          className={`${geistSans.variable} ${geistMono.variable} home-background antialiased overflow-hidden`}
+        >
+          <main>{children}</main>
+          </body>
+      </html>
+    </StoreProvider>
+  );
+}
